ee0988b573ece2245527ef5d484cd5034be9d624
kimhoyeon <hoyeon7043@icloud.com>
2025-03-28 14:03:20 +0900

    4주차 과제

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+sel = int(input("1. 입력한 수식 계산 2. 두 수 사이의 합계 :"))
+if sel == 1 :
+    num = input("*** 수식을 입력하세요 :")
+    result = eval(num)
+    print(f"{num} 결과는 {result}입니다.")
+
+else :
+    num1 = int(input("*** 첫 번째 숫자를 입력하세요 :"))
+    num2 = int(input("*** 두 번째 숫자를 입력하세요 :"))
+    result =sum(range(num1, num2 + 1))
+    print(f"{num1}+...+{num2}는 {result}입니다.")
+
